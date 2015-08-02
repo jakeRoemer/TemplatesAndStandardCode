d2fd0ff377e714cd037ab6634ffd74bda15476e1
Ellevol <joshlovelle@gmail.com>
2015-08-01 23:22:01 -0400

    Logic for Rook.java, move method also pushing knight.class for jake's problem

--- Templates and Standard Code/src/chessForTwo/Rook.java
+++ Templates and Standard Code/src/chessForTwo/Rook.java
@@ -5,9 +5,18 @@ public class Rook extends Piece {
 	public Rook(boolean color, int identifier) {
 		super("Rook" + identifier, "R", color, identifier);
 	}
-	
+
 	@Override
 	public boolean move(int newFile, int newRank) {
+		if ((Math.abs(rank - newRank) > 0 && Math.abs(file - newFile) == 0)
+				|| (Math.abs(rank - newRank) == 0 && Math.abs(file - newFile) > 0)) {
+			if (realSquare(newFile, newRank)) {
+				file = newFile;
+				rank = newRank;
+				return true; // must change pieces on board as well
+			}
+		}
+		System.out.println("Can not move piece there");
 		return false;
 	}
 
