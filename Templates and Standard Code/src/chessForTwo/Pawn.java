c2d582e72e49a9b68a6315329cdddc4de09d70e1
Ellevol <joshlovelle@gmail.com>
2015-08-02 01:27:58 -0400

    Rewrite Pawn.java move method. allowed for pawn first move to be +2 Rank and set enPassant = true;
    still need to update enpassant to false after opponents next turn

--- Templates and Standard Code/src/chessForTwo/Pawn.java
+++ Templates and Standard Code/src/chessForTwo/Pawn.java
@@ -1,30 +1,49 @@
 package chessForTwo;
 
 public class Pawn extends Piece {
-	
+
+	public Boolean enPassant = false;
+
 	public Pawn(boolean color, int identifier) {
 		super("Pawn" + identifier, "P", color, identifier);
 	}
-	
+
 	@Override
 	public boolean move(int newFile, int newRank) {
-		if (getRank() + 1 != newRank) {
-			System.out.println("Can not move piece there");
+		if (color /* white */ && Math.abs(getFile() - newFile) == 0 && (realSquare(newFile, newRank))) {
+			if (newRank == getRank() + 1) {
+				setFile(newFile);
+				setRank(newRank);
+				return true; // must change pieces on board as well
+			} else if (newRank == getRank() + 2 && getRank() == 2) {
+				setFile(newFile);
+				setRank(newRank);
+				enPassant = true;
+				return true; // must change pieces on board as well
+			}
+		} else if (Math.abs(getFile() - newFile) == 0 && (realSquare(newFile, newRank))) {
+			if (newRank == getRank() - 1) {
+				setFile(newFile);
+				setRank(newRank);
+				return true; // must change pieces on board as well
+			} else if (newRank == getRank() - 2 && getRank() == 7) {
+				setFile(newFile);
+				setRank(newRank);
+				enPassant = true;
+				return true; // must change pieces on board as well
+			}
 		}
-		if (realSquare(newFile, newRank)) {
-			setFile(newFile);
-			setRank(newRank);
-			return true; //must change pieces on board as well					
-		}
-		return true;
+		System.out.println("Can not move piece there");
+		return false;
+
 	}
-	
+
 	@Override
 	public void startingPosition() {
 		if (color) {
-			setRank(1);
+			setRank(2);
 		} else {
-			setRank(8);
+			setRank(7);
 		}
 		setFile(id);
 	}
