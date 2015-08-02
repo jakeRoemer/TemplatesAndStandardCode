fbad3a9fd385d834f6135c83e35b2430157a6bcc
Ellevol <joshlovelle@gmail.com>
2015-08-01 20:32:56 -0400

    Knight.jave move class logic implemented

--- Templates and Standard Code/src/chessForTwo/Bishop.java
+++ Templates and Standard Code/src/chessForTwo/Bishop.java
@@ -1,17 +1,24 @@
 package chessForTwo;
 
-public class Bishop extends Piece{
+public class Bishop extends Piece {
 
 	public Bishop(boolean color, int identifier) {
-		super("Bishop" + identifier , "B", color, identifier);
+		super("Bishop" + identifier, "B", color, identifier);
 	}
-	
+
 	@Override
-	public boolean move (int newFile, int newRank) {
-		
+	public boolean move(int newFile, int newRank) {
+		if ((Math.abs(rank - newRank) == Math.abs(file - newFile))) {
+			if (realSquare(newFile, newRank)) {
+				file = newFile;
+				rank = newRank;
+				return true; // must change pieces on board as well
+			}
+		}
+		System.out.println("Can not move piece there");
 		return false;
 	}
-	
+
 	@Override
 	public void startingPosition() {
 		if (color) {
