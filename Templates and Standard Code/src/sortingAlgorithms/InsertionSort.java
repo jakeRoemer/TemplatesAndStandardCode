f8d15981e2554feea36a8e1136bdffd1c4191698
jakeRoemer <tuc42833@temple.edu>
2015-08-04 18:50:43 -0400

    Fixed insertion sort Created merge sort Created concurrent merge sort - concurrent merge sort simply runs mergeSort recursive call in a new thread. A further extension to merge method can be made to multithreaded programming

--- Templates and Standard Code/src/sortingAlgorithms/InsertionSort.java
+++ Templates and Standard Code/src/sortingAlgorithms/InsertionSort.java
@@ -1,48 +1,31 @@
 package sortingAlgorithms;
 
-import java.util.Random;
-
 public class InsertionSort {
 	public static void main(String [] args) {
 		int [] totalSyncCount = new int[5];
+		createArray.randomizedIntArray(totalSyncCount,100);
+		for (int i = 0; i < totalSyncCount.length; i++) {
+			System.out.print(totalSyncCount[i] + " ");
+		}
+		System.out.println();
 		insertSort(totalSyncCount);
 		for (int i = 0; i < totalSyncCount.length; i++) {
-			System.out.println(totalSyncCount[i]);
+			System.out.print(totalSyncCount[i] + " ");
 		}
+		System.out.println();
 	}
 	
-	//creates new numbers to insert into array while keeping the array sorted [smallest to largest]
 	public static void insertSort(int [] totalSyncCount) {
-		int val;
-		Random rand = new Random(System.currentTimeMillis());
-	    for (int k = 0; k < totalSyncCount.length; k++) {
-	      //can not switch on zero acquire
-	      val = rand.nextInt(100)+1;
-	      for (int i = totalSyncCount.length-1; i >= 0; i--) {
-				if (val >= totalSyncCount[i]) {
-					for (int j = 1; j <= i; j++) {
-						int temp = totalSyncCount[j];
-						totalSyncCount[j] =  totalSyncCount[j-1];
-						totalSyncCount[j-1] = temp;
-					}
-					totalSyncCount[i] = val;
-					break;
-				}
+		for (int i = 1; i < totalSyncCount.length; i++) {
+			int val = totalSyncCount[i];
+			int j = 0;
+			while (j < i && val >= totalSyncCount[j]) {
+				j++;
 			}
-	    }
-	}
-	
-	public static void insert(int [] totalSyncCount, int val) {
-		for (int i = totalSyncCount.length-1; i >= 0; i--) {
-			if (val >= totalSyncCount[i]) {
-				for (int j = 1; j <= i; j++) {
-					int temp = totalSyncCount[j];
-					totalSyncCount[j] =  totalSyncCount[j-1];
-					totalSyncCount[j-1] = temp;
-				}
-				totalSyncCount[i] = val;
-				break;
+			for (int k = i-1; k >= j; k--) {
+				totalSyncCount[k+1] = totalSyncCount[k];
 			}
+			totalSyncCount[j] = val;
 		}
 	}
 }
---
+++ Templates and Standard Code/src/sortingAlgorithms/createArray.java
@@ -0,0 +1,13 @@
+package sortingAlgorithms;
+
+import java.util.Random;
+
+public class createArray {
+
+	public static void randomizedIntArray (int [] A, int dataSizeLimit) {
+		Random rand = new Random(System.currentTimeMillis());
+		for (int i = 0; i < A.length; i++) {
+			A[i] = rand.nextInt(dataSizeLimit);
+		}
+	}
+} 
